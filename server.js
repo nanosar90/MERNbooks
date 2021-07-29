14ecee683b4a38cae663ab7c580da306da4253c7
Nathan Nosar <nathan.nosar@gmail.com>
2021-07-29 14:10:48 -0700

    please work

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const cors = require('cors');
-const path = require('path')
+const path = require('path');
 const app = express();
 const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+
+const routes = require('./client/server/routes')
 
 // Connection to the MongoDB Database
-require('./server/db');  // OR
+/* const server = require('./server/db'); */  // OR
 // require('./database');
 
 app.use(cors());
@@ -20,14 +23,22 @@ app.use(express.json());
 //     res.send("Hello world")
 // })
 
-app.use(express.static(path.join(__dirname, '../build')))
+app.use(express.static(path.join(__dirname, '../client/build')))
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build'))
+    res.sendFile(path.join(__dirname, '../client/build'))
 })
 
-app.use(require("./server/routes"));
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+const db = mongoose.connect(`mongodb+srv://nathan:password1234@cluster0.dfgvs.mongodb.net/googleBooks?retryWrites=true&w=majority`, {
+    useNewUrlParser: true,
+    useFindAndModify: false
+})
+.then(() => console.log("MongoDB Database is Connected"))
+.catch((err) => console.log(`An error has occured in DB connection: ${err}`))
+
+app.use(require("./client/server/routes"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(process.env.PORT, () => {
+    console.log(`Server started on port ${PORT}`);
 });
