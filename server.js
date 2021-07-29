96fd696a3dad4e834f05ca562dde31b07f521a43
Nathan Nosar <nathan.nosar@gmail.com>
2021-07-29 16:10:32 -0700

    files

--- server.js
+++ server.js
@@ -23,9 +23,9 @@ app.use(express.json());
 //     res.send("Hello world")
 // })
 
-app.use(express.static(path.join(__dirname, '../client/build')))
+app.use(express.static(path.join(__dirname, './client/build')))
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build'))
+    res.sendFile(path.join(__dirname, './client/build'))
 })
 
 
